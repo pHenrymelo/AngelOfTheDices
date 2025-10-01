192ce7304ff6aadebf4346b5ff0eff28fb04ea52
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-10-01 14:56:15 -0300

    feat: Initial commit

---
+++ src/test/java/dev/kaiserInc/AngelOfTheDices/AngelOfTheDicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kaiserInc.AngelOfTheDices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelOfTheDicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
