c7cc08e7569b9e8a6884b7e0b4315d114b74cbb6
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-10-02 21:32:00 -0300

    fix: change integers decorator in request DTO

--- src/main/java/dev/kaiserInc/AngelOfTheDices/character/dto/CharacterCreateRequestDTO.java
+++ src/main/java/dev/kaiserInc/AngelOfTheDices/character/dto/CharacterCreateRequestDTO.java
@@ -1,20 +1,21 @@
 package dev.kaiserInc.AngelOfTheDices.character.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CharacterCreateRequestDTO(
         @NotBlank String name,
         @NotBlank String origin,
         @NotBlank String char_class,
-        @NotBlank Integer nex,
-        @NotBlank Integer strength,
-        @NotBlank Integer agility,
-        @NotBlank Integer presence,
-        @NotBlank Integer intellect,
-        @NotBlank Integer vigor,
-        @NotBlank Integer maxHitPoints,
-        @NotBlank Integer maxEffortPoints,
-        @NotBlank Integer maxSanity,
+        @NotNull Integer nex,
+        @NotNull Integer strength,
+        @NotNull Integer agility,
+        @NotNull Integer presence,
+        @NotNull Integer intellect,
+        @NotNull Integer vigor,
+        @NotNull Integer maxHitPoints,
+        @NotNull Integer maxEffortPoints,
+        @NotNull Integer maxSanity,
         @NotBlank String rank,
-        @NotBlank Integer prestigePoints
+        @NotNull Integer prestigePoints
 ) {}
