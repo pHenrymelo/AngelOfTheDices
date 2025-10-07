7008b3c6c649489d4a8b7ecb081acc81dbb88656
pHenrymelo <pedrohenriquebmelo25@gmail.com>
2025-10-07 20:11:48 -0300

    fix: increment char class smart enum

--- src/main/java/dev/kaiserInc/AngelOfTheDices/character/classPath/CharacterClass.java
+++ src/main/java/dev/kaiserInc/AngelOfTheDices/character/classPath/CharacterClass.java
@@ -1,8 +1,24 @@
 package dev.kaiserInc.AngelOfTheDices.character.classPath;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum CharacterClass {
-    COMBATANT,
-    SPECIALIST,
-    OCCULTIST,
-    SURVIVOR
+
+    COMBATANT("Combatente"),
+    SPECIALIST("Especialista"),
+    OCCULTIST("Ocultista"),
+    SURVIVOR("Sobrevivente");
+
+    private final String displayName;
+
+    CharacterClass(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getName(){
+        return this.name();
+    }
 }
